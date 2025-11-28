07750abf67c7f85933731ff0f952ee1be543a5b2
BeiMingXiaoYu255 <2354957009@qq.com>
2025-11-28 16:21:44 +0800

    完成ScoreDao和其实现

--- src/main/java/com/cjlu/dao/ScoreDao.java
+++ src/main/java/com/cjlu/dao/ScoreDao.java
@@ -1,5 +1,20 @@
 package com.cjlu.dao;
 
-public class ScoreDao {
+import java.util.Map;
 
+public interface ScoreDao {
+    //创建成绩表
+    public void createScoreTable();
+    //添加成绩记录
+    public void addScoreRecord(int studentId, String courseCode, double score);
+    //删除成绩记录
+    public void deleteScoreRecord(int studentId, String courseCode);
+    //更新成绩记录
+    public void updateScoreRecord(int studentId, String courseCode, double newScore);
+    //根据学生ID和课程代码获取成绩记录
+    public Integer getScoreRecord(int studentId, String courseCode);
+    //查询某学生的所有成绩记录
+    public Map<String, Double> getAllScoresByStudentId(int studentId);
+    //查询某课程的所有学生成绩记录
+    public Map<Integer, Double> getAllScoresByCourseCode(String courseCode);
 }
